36f5f85b6192bdb286a5b4ce7f50a67315c7da15
elira <elira@DESKTOP-40OR9PA>
2018-01-21 22:14:59 +0200

    Draw  traffic light

---
+++ src/maman16aclient/Maman16aClient.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maman16aclient;
+
+/**
+ *
+ * @author elira
+ */
+public class Maman16aClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
